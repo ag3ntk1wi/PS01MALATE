3cccefeef41d66e5f07f5cc85a5edb26863c712e
jramilm <malatejramil@gmail.com>
2024-08-26 21:51:06 +0800

    commit 1

--- PS01_5_SwitchDays/Program.cs
+++ PS01_5_SwitchDays/Program.cs
@@ -10,6 +10,30 @@ namespace PS01_5_SwitchDays
     {
         static void Main(string[] args)
         {
+            const int PI = 3.14;
+
+            Console.Write("Enter Radius: ");
+            int rad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("P : Perimeter ; A : Area ; X : Exit");
+            char compute = Convert.ToChar(Console.ReadLine());
+
+            switch(compute)
+            {
+                case 'P':
+                    double pmeter = 2 * PI * rad;
+                    Console.WriteLine("The Perimeter is " + pmeter);
+                    break;
+                case 'A':
+                    double area = PI * (rad ** 2);
+                    Console.WriteLine("The Area is " + area);
+                    break;
+                case 'X':
+                    Console.WriteLine("Thank You.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option.");
+                    break;
+            }
         }
     }
 }
